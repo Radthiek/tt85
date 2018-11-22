58eb22a46ddfd6bbfd7a46a26e35115e4f860cbf
Radthiek <44319561+Radthiek@users.noreply.github.com>
2018-11-22 13:16:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -2481,7 +2481,25 @@ client.on('messageUpdate', (oldRebel, newRebel) => {
     }
 });
 
+const invites = {};
+const wait = require('util').promisify(setTimeout);
+client.on('ready', () => {
+  wait(1000);
 
+  client.guilds.forEach(g => {
+    g.fetchInvites().then(guildInvites => {
+      invites[g.id] = guildInvites;
+    });
+  });
+});
+
+let sWlc = JSON.parse(fs.readFileSync("./setWlc.json", "UTF8"))   
+client.on('message', message => {
+if(message.channel.type === "dm") return;
+if(message.author.bot) return;
+  if(!sWlc[message.guild.id]) sWlc[message.guild.id] = {
+    channel: "word"
+}
 const channel = sWlc[message.guild.id].channel
   if (message.content.startsWith(prefix + "setwelcomer")) {
     if(!message.member.hasPermission(`MANAGE_GUILD`)) return;
